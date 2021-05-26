05964bb357a6d9b62421235be4d5b647fcfe4ece
taese0ng <taese0ng@naver.com>
2021-05-26 18:30:23 +0900

    change checKorNum to checkNickName

--- customCheck.ts
+++ customCheck.ts
@@ -1,7 +1,7 @@
 import {checkOnlyNum} from './checkNumber';
 
 // 음절과 숫자로만 이루어져있으며, 숫자로만은 이루어지지 않는지 그리고 10글자 이하인지 체크
-export const checkKorNum = (data: string) => {
+export const checkNickName = (data: string) => {
     const regexp = /(^[가-힣|0-9]{1,10})+$/;
 
     if(checkOnlyNum(data)){
